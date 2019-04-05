6fae56e21cb73b691727187e17fb7a9a69c79d4b
Liybov <43754643+Liybov@users.noreply.github.com>
2019-04-05 13:01:19 +0300

    Create header.php

---
+++ test2/php/blocks/header.php
@@ -0,0 +1,23 @@
+<div class="top-bar">
+             <div class="off-canvas-wrapper">
+              <div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
+                <div class="off-canvas position-left" id="offCanvasLeft" data-off-canvas>
+                    <ul class="vertical menu">
+                      <li><a href="#">Головна</a></li>
+                      <li><a href="#">Переваги</a></li>
+                      <li><a href="#">Про нас</a></li>
+                      <li><a href="#">Курси</a></li>
+                      <li><a href="../contact-us.html">Контакти</a></li>
+                    </ul>
+                </div>
+                <div class="off-canvas-content" data-off-canvas-content>
+                  <div class="title-bar">
+                    <div class="title-bar-left">
+                      <button class="menu-icon" type="button" data-open="offCanvasLeft"></button>
+                        <span class="title-bar-title">ТУТ ЛОГОТИП</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+    </div>
